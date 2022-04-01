2629a81614a8b8c517829e08a524dd8a067de26f
Tee Chong Sheng <56125177+tee0106@users.noreply.github.com>
2022-04-01 21:02:34 +0800

    Add files via upload

---
+++ C# socket server/GameSocket/frm_Server.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace GameSocket
+{
+    partial class frm_Server
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lb_stt = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.list_Client = new System.Windows.Forms.CheckedListBox();
+            this.txt_Text = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.rich_Text = new System.Windows.Forms.RichTextBox();
+            this.statusStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lb_stt});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 261);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(457, 22);
+            this.statusStrip1.TabIndex = 0;
+            // 
+            // lb_stt
+            // 
+            this.lb_stt.Name = "lb_stt";
+            this.lb_stt.Size = new System.Drawing.Size(41, 17);
+            this.lb_stt.Text = "ready";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.list_Client);
+            this.groupBox1.Location = new System.Drawing.Point(232, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(211, 211);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "List Client";
+            // 
+            // list_Client
+            // 
+            this.list_Client.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.list_Client.FormattingEnabled = true;
+            this.list_Client.Location = new System.Drawing.Point(3, 17);
+            this.list_Client.Name = "list_Client";
+            this.list_Client.Size = new System.Drawing.Size(205, 191);
+            this.list_Client.TabIndex = 5;
+            // 
+            // txt_Text
+            // 
+            this.txt_Text.Location = new System.Drawing.Point(15, 228);
+            this.txt_Text.Name = "txt_Text";
+            this.txt_Text.Size = new System.Drawing.Size(205, 21);
+            this.txt_Text.TabIndex = 3;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(237, 226);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 21);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // rich_Text
+            // 
+            this.rich_Text.Location = new System.Drawing.Point(18, 12);
+            this.rich_Text.Name = "rich_Text";
+            this.rich_Text.Size = new System.Drawing.Size(202, 205);
+            this.rich_Text.TabIndex = 5;
+            this.rich_Text.Text = "";
+            // 
+            // frm_Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 283);
+            this.Controls.Add(this.rich_Text);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txt_Text);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "frm_Server";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Server";
+            this.Load += new System.EventHandler(this.frm_Server_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lb_stt;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txt_Text;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.CheckedListBox list_Client;
+        private System.Windows.Forms.RichTextBox rich_Text;
+    }
+}
+
---
+++ C# socket server/GameSocket/frm_Server.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GameSocket
+{
+
+    public class SocketT2h
+    {
+        public Socket _Socket { get; set; }
+        public string _Name { get; set; }
+        public SocketT2h(Socket socket)
+        {
+            this._Socket = socket;
+        }
+    }
+    public partial class frm_Server : Form
+    {
+        private  byte[] _buffer = new byte[1024];
+        public List<SocketT2h> __ClientSockets { get; set; }
+        List<string> _names = new List<string>();
+        private  Socket _serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        public frm_Server()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+            __ClientSockets = new List<SocketT2h>();
+        }
+
+        private void frm_Server_Load(object sender, EventArgs e)
+        {
+            SetupServer();
+        }
+        private  void SetupServer()
+        {
+            _serverSocket.Bind(new IPEndPoint(IPAddress.Any, 100));
+            _serverSocket.Listen(1);
+            _serverSocket.BeginAccept(new AsyncCallback(AppceptCallback), null);
+        }
+        private  void AppceptCallback(IAsyncResult ar)
+        {
+            Socket socket = _serverSocket.EndAccept(ar);
+            __ClientSockets.Add(new SocketT2h(socket));
+            list_Client.Items.Add(socket.RemoteEndPoint.ToString());
+
+            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+            _serverSocket.BeginAccept(new AsyncCallback(AppceptCallback), null);
+        }
+        
+        private  void ReceiveCallback(IAsyncResult ar)
+        {
+            
+            Socket socket = (Socket)ar.AsyncState;
+            if (socket.Connected)
+            {
+                int received;
+                try
+                {
+                    received = socket.EndReceive(ar);
+                }
+                catch (Exception)
+                {
+                    for (int i = 0; i < __ClientSockets.Count; i++)
+                    {
+                        if (__ClientSockets[i]._Socket.RemoteEndPoint.ToString().Equals(socket.RemoteEndPoint.ToString()))
+                        {
+                            __ClientSockets.RemoveAt(i);
+                        }
+                    }
+                    return;
+                }
+                if (received!=0)
+                {
+                    byte[] dataBuf = new byte[received];
+                    Array.Copy(_buffer, dataBuf, received);
+                    string text = Encoding.UTF8.GetString(dataBuf);
+                    lb_stt.Text = "Text received: " + text;
+                    string reponse = string.Empty;
+
+                    for (int i = 0; i < __ClientSockets.Count; i++)
+                    {
+                        if (socket.RemoteEndPoint.ToString().Equals(__ClientSockets[i]._Socket.RemoteEndPoint.ToString()))
+                        {
+                            String str = __ClientSockets[i]._Socket.RemoteEndPoint.ToString();
+                            int find = str.IndexOf(":",0);
+                            String str_ip = str.Substring(0, find);
+                            rich_Text.AppendText(str_ip + " : " + text + "\n");
+                            if (text=="Disconnected!")
+                            {
+                                list_Client.Items.Remove(__ClientSockets[i]._Socket.RemoteEndPoint.ToString());
+                            }
+                            if (text.Contains("##"))
+                            {
+                                for (int k = 0; k < __ClientSockets.Count; k++)
+                                {
+                                    if (list_Client.Items[k].Equals(__ClientSockets[i]._Socket.RemoteEndPoint.ToString()))
+                                    {
+                                        list_Client.SetItemChecked(k, false);
+                                    }
+                                    else
+                                    {
+                                        list_Client.SetItemChecked(k, true);
+                                    }
+                                }
+                                for (int k = 0; k < list_Client.Items.Count; k++)
+                                {
+                                    if (list_Client.GetItemCheckState(k) == CheckState.Checked)
+                                    {
+                                        Sendata(__ClientSockets[k]._Socket, text);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    
+                }
+                else
+                {
+                    for (int i = 0; i < __ClientSockets.Count; i++)
+                    {
+                        if (__ClientSockets[i]._Socket.RemoteEndPoint.ToString().Equals(socket.RemoteEndPoint.ToString()))
+                        {
+                            __ClientSockets.RemoveAt(i);
+                        }
+                    }
+                }
+            }
+            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+        }
+        void Sendata(Socket socket,string noidung)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(noidung);
+            socket.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(SendCallback), socket);
+            _serverSocket.BeginAccept(new AsyncCallback(AppceptCallback), null);
+        }
+        private  void SendCallback(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            socket.EndSend(AR);
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if(txt_Text.Text != "")
+            {
+                for (int i = 0; i < list_Client.Items.Count; i++)
+                {
+                    if (list_Client.GetItemCheckState(i) == CheckState.Checked)
+                    {
+                        Sendata(__ClientSockets[i]._Socket, txt_Text.Text);
+                    }
+                }
+                rich_Text.AppendText("Server: " + txt_Text.Text + "\n");
+                txt_Text.Text = "";
+            }
+        }
+    }
+}
